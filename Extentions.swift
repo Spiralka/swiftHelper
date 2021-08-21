a710f47057919e7489cf1552011e0c6c35c14b4b
Evgeniy <spiralcka@gmail.com>
2021-08-21 22:47:37 +0300

    Add files via upload

---
+++ Extentions.swift
@@ -0,0 +1,18 @@
+//
+//  Extentions.swift
+//  HomeWork
+//
+//  Created by Smetanka on 21.08.2021.
+//
+
+import UIKit
+
+extension ViewController {
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
